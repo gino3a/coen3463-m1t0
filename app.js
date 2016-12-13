afac36fd0edf71d46387e0ac2d0d7e5bdefb7838
ginotria <romanangelo.tria@gmail.com>
2016-12-13 13:02:07 +0800

    deleted node modules

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('port', (process.env.PORT || 3000));
 
 // set static path middleware
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(app.get('port'), function() {
     console.log('Expres server is running on port 3000!');
 });
