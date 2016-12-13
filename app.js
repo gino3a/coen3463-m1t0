4d9482b777e5940868069222ebe86389d923889e
ginotria <romanangelo.tria@gmail.com>
2016-12-13 12:57:04 +0800

    fixed port number

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+app.set('port', app.env.PORT || 3000)
 
 // set static path middleware
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.listen('3000', function() {
+app.listen(app.get('port'), function() {
     console.log('Expres server is running on port 3000!');
 });
